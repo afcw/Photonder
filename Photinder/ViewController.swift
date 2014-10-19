603a3f817bc6fddc21d8ed76aa22dea7e917d6fd
Deepak Bhagchandani <deepak.bhagchandani@pearson.com>
2014-10-19 14:53:22 -0700

    initial commit - deepak

--- Photinder/ViewController.swift
+++ Photinder/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    var center : CGPoint = CGPoint()
+    var alpha : CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+        self.center = self.imageView.center
+        self.alpha = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func imagePan(sender: UIPanGestureRecognizer) {
+        
+        
+    
+        
+        var translation = sender.translationInView(self.imageView)
 
+        self.imageView.center.x = self.center.x + translation.x
+        //self.imageView.center.y = self.center.y + translation.y
+        
+       // self.imageView.transform = CGAffineTransformMakeTranslation(x, y)
+  
+        self.imageView.alpha = (1 - abs(translation.x/(self.view.bounds.size.width/2))) + 0.5
+        
+        var distanceToTranslate = abs(translation.x/(self.view.bounds.size.width/2))
+        
+        var halfDistance = self.view.bounds.size.width/2
+        var rotateBy = translation.x/halfDistance
+        
+        self.imageView.transform = CGAffineTransformMakeRotation(rotateBy)
+        
+        
+    }
+  
 }
-
